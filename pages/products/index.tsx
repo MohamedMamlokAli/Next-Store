94c9c10cce9ec8bd4b17a51d867d7db147c6c135
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2021-08-27 12:24:27 +0200

    Fetched the Products data in the Products page to avoid blank page on refresh

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,28 +1,49 @@
 import Link from 'next/link';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { State } from '../../state';
+import ProductsFilter from '../../components/CoreComponents/ProductsFilter/ProductsFilter';
+import styled from 'styled-components';
+import ProductsGrid from '../../components/CoreComponents/ProductsGrid/ProductsGrid';
+import { ProductData } from '..';
+import { InferGetStaticPropsType } from 'next';
+//Styled Components
+const ProductsPage = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
 
-const Products = () => {
-  const productsData = useSelector((store: State) => store.products);
+const ProductsAndFiltersContainer = styled.div`
+  max-width: 1150px;
+  position: relative;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-bottom: 10rem;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    padding-bottom: 4rem;
+  }
+`;
+
+const Products = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div>
-      <div>
-        {productsData.map((item, index) => {
-          return (
-            <Link
-              key={index}
-              href='/products/[id]'
-              as={`/products/${item.id}`}
-              passHref
-            >
-              <a> {item.title}</a>
-            </Link>
-          );
-        })}
-      </div>
-    </div>
+    <ProductsPage>
+      <h1>Home / Products</h1>
+      <ProductsAndFiltersContainer>
+        <ProductsFilter />
+        <ProductsGrid ProductInfo={data} />
+      </ProductsAndFiltersContainer>
+    </ProductsPage>
   );
+};
+export const getStaticProps = async () => {
+  const res = await fetch('https://fakestoreapi.com/products/');
+  const data: ProductData[] = await res.json();
+  return {
+    props: { data },
+  };
 };
 
 export default Products;
