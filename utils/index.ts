115602564e8f2158b2606da7ea77b3f41cdf60a5
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2021-08-27 17:45:52 +0200

    Added more functions

--- utils/index.ts
+++ utils/index.ts
@@ -7,3 +7,27 @@ export const returnFeaturedProducts = (products: ProductData[]) => {
 export const returnAllproducts = (products: ProductData[]) => {
   return products;
 };
+
+export const returnCategories = (products: ProductData[]) => {
+  const duplicatedCategories: string[] = products.map((item) => item.category);
+  const uniqueCategories: string[] = [...new Set(duplicatedCategories)];
+  return uniqueCategories;
+};
+
+export const capitalize = (name: string) => {
+  return name[0].toUpperCase() + name.slice(1);
+};
+export const sortPrices = (products: ProductData[]) => {
+  const duplicatedprices: number[] = products.map((item) => item.price);
+  const uniqueprices: number[] = [...new Set(duplicatedprices)];
+  return uniqueprices.sort((firstEl, secondEl) => firstEl - secondEl);
+};
+export const minPrice = (products: ProductData[]) => {
+  const min = sortPrices(products)[0];
+  return min;
+};
+export const maxPrice = (products: ProductData[]) => {
+  const prices = sortPrices(products);
+  const max = prices[prices.length - 1];
+  return max;
+};
